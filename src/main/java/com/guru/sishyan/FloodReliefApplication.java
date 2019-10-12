0627fe3713cc313bfd4e7e458161a8fef23b3978
Arunkumar Muthusamy <arunkumar.muthusamy@metapack.com>
2019-10-12 15:51:03 +0530

    Package Created

---
+++ src/main/java/com/guru/sishyan/FloodReliefApplication.java
@@ -0,0 +1,11 @@
+package com.guru.sishyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodReliefApplication {
+    public static void main(String args[]){
+        SpringApplication.run(FloodReliefApplication.class,args);
+    }
+}
---
+++ src/main/java/com/guru/sishyan/controller/ResourceManagement.java
@@ -0,0 +1,12 @@
+package com.guru.sishyan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceManagement {
+    @RequestMapping(value = "/")
+    public void addVolunteer(){
+
+    }
+}
