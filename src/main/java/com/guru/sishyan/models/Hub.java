ff939f7a088e98ae18e6fc498d27f3c5dc474443
kesavanpalani <kesavanp@sahajsoft.com>
2019-10-12 16:25:06 +0530

    created model for volunteer

---
+++ src/main/java/com/guru/sishyan/models/Hub.java
@@ -0,0 +1,5 @@
+package com.guru.sishyan.models;
+
+public class Hub {
+    String Location;
+}
---
+++ src/main/java/com/guru/sishyan/models/Volunteer.java
@@ -0,0 +1,9 @@
+package com.guru.sishyan.models;
+
+
+public class Volunteer {
+    String Location;
+    String Landmark;
+    String[] phoneNumbers;
+    Integer peopleCount;
+}
